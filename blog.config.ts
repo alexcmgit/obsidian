aac7e71c94562efff0bf28ac26453432f3eab7d8
Alex Castro <git@alexcastro.dev>
2025-11-20 16:09:22 -0300

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -6,7 +6,7 @@ Fork this blog https://alexcmgit.github.io/obsidian/how-to-use-obsidian's-blog-t
 export default {
   owner: "alexcmgit",
   repo: "obsidian",
-  pathPrefix: "/obsidian",
+  pathPrefix: "/",
   // This will prevent someone else creating a discussion and publishing to your blog without your authorization.
   // Only users with repo-write access can modify announcement-type categories.
   safeCategories: [`Published`],
@@ -15,7 +15,7 @@ export default {
     twitterUsername: `<your-tt-username>`,
     description: `Obsidian is a blog and template built with Gatsby and GitHub, free and open-source.`,
     image: `https://user-images.githubusercontent.com/51419598/240399860-a9fb362c-28a0-4838-b8d5-8332c2feecb5.png`,
-    siteUrl: `https://alexrintt.github.io`,
+    siteUrl: `https://obsidian.alexisa.dev`,
   },
   socialMedia: socialMedia,
   postsPerPage: 10,
